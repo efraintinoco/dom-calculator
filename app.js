831cb7706d2136206437ee38353cb0b249b3ec22
efraintinoco <oakcliff_21@yahoo.com>
2022-04-07 16:21:50 -0500

    add functions

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
 const screen = document.querySelector("#screen")
 const buttons = document.querySelectorAll("span")
 const clear = document.querySelector("#clear")
-const operatorsBtn = document.querySelectorAll(".operator")
-const numbersBtn = document.querySelectorAll(".number")
+let acceptingInput = true
 
-
-numbersBtn.forEach(number => {
-    number.addEventListener("click", (event) => {
-        screen.innerText += event.target.innerText
-
-    })
-})
-
-
-operatorsBtn.forEach(operator => {
-    operator.addEventListener("click", (event) => {
-        screen.textContent += event.target.textContent
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        if (button.textContent == "÷" && acceptingInput) {
+            screen.textContent += "/"
+        }
+        if (button.textContent == "x" && acceptingInput) {
+            screen.textContent += "*"
+        }
+        if (button.textContent === "C" && acceptingInput) {
+            screen.textContent = ""
+        }
+        if (button.textContent !== "÷" && button.textContent !== "=" && button.textContent !== "x" && button.textContent !== "C" && acceptingInput) {
+            screen.textContent += button.textContent
+        }
+        try {
+            if (button.textContent === "=" && acceptingInput) {
+                if (screen.textContent.includes("/0")) {
+                    screen.textContent = "Error, Resetting"
+                }
+                screen.textContent = eval(screen.textContent)
+            }
+        } catch (error) {
+            screen.textContent = "Error, Resetting"
+            acceptingInput = false
+            setTimeout(function() {
+                acceptingInput = true
+                screen.textContent = ""
+            }, 1800)
+        }
     })
 })
